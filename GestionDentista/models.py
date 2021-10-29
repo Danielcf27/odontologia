38adbefbd2caa652c1cc5005e336783c6f9144b0
DanielC <daniel14458@gmail.com>
2021-10-29 09:22:16 -0600

    Modelo ejemplo

--- GestionDentista/models.py
+++ GestionDentista/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.db.models.base import Model
 
 # Create your models here.
+class Clientes(models.Model):
+    Nombre= models.TextField(max_length=30)
+    Apellido= models.TextField(max_length=30)
+    Telefono= models.IntegerField() 
+    Direccion= models.TextField(max_length=100)
